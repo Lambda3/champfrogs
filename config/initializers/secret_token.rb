3b1a50b1d69a0843c99b99705a1d28086c73a360
Victor Hugo Germano <victorhg@gmail.com>
2013-10-19 13:34:03 -0300

    first commit html layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Champfrogs::Application.config.secret_token = 'a2a82e506376a64cdee3133fac9bcf1e498a6df1dbf6da7dcf8fdf6e110ecbbd04f639601f042b572cfda4af593c4590c6dfefcf06ca0a84534c19f2cc9b77b0'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Champfrogs::Application.routes.draw do
+  root :to => 'home#index'
+end
