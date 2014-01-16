2205ba9e73cf7ca5dbac0bf1a541930222272b07
Raphael Molesim <raphael.sm86@gmail.com>
2014-01-16 10:31:52 -0200

    Mudei o algoritmo de distancia de motivadores

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,28 +6,10 @@ class Answer < ActiveRecord::Base
   end
   
   def distance(answer) # levenshtein_distance
-    size = self.motivators.size
-    
-    #create temp matrix
-    temp_matrix = Array.new(size+1) { Array.new(size+1) }
-    (0..size).each do |i| 
-      temp_matrix[i][0] = i
-      temp_matrix[0][i] = i
-    end
-    
-    (1..size).each do |j|
-      (1..size).each do |i|
-        temp_matrix[i][j] = if self.motivators[i-1] == answer.motivators[j-1]  # adjust index into string
-                              temp_matrix[i-1][j-1]       # no operation required
-                            else
-                              [ temp_matrix[i-1][j]+1,    # deletion
-                                temp_matrix[i][j-1]+1,    # insertion
-                                temp_matrix[i-1][j-1]+1,  # substitution
-                              ].min
-                            end
-      end
-    end
-    temp_matrix[size][size]
+    motivators.map.with_index do |item, index|
+      i = answer.motivators.index(item)
+      (i - index).abs
+    end.inject(0) { |r,i| r + i}
   end
   
   class << self
